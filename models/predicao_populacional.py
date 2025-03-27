03663bd54b22d84c2bd7c2e4b97424fc4575dd39
Deolia7 <alan.agrogsia@gmail.com>
2025-03-27 13:10:45 -0300

    Delete models directory

--- models/predicao_populacional.py
+++
@@ -1,9 +0,0 @@
-def prever_populacao(dados_pontos, clima):
-    base = sum(p["adultos"] + p["ninfas"] for p in dados_pontos) / len(dados_pontos)
-    previsao = []
-    for i in range(10):
-        clima_dia = clima["list"][min(i, len(clima["list"]) - 1)]
-        temp = clima_dia["main"]["temp"]
-        pop = base * (1 + (temp - 20) * 0.03)
-        previsao.append(round(pop, 2))
-    return previsao
